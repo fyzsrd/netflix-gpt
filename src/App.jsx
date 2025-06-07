83e2c3c11d05619c4e4db55cb3954abafe0576d2
fyzsrd <fyzsrd@gmail.com>
2025-06-07 07:12:11 +0530

    Initial commit with full project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='bg-gray-950 h-screen p-1.5'>
+      <h1 className='text-sky-100 text-2xl font-bold'>its me nun otherthan me</h1>
+    </div>
+      </>
+  )
+}
+
+export default App
